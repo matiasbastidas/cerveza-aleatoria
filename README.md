11fcd21697465d46b048675bab32cf76727a345a
matiasbastidas <matias.bastidasm@gmail.com>
2022-08-17 18:38:04 -0400

    cmejorasÂ

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# cerveza-aleatoria
-cerveza-aleatoria
+# Generador Aleatorio de cervezas
+
+Proyecto que muestra cervezas de manera aleatoria. Los datos son obtenidos a través de una API pública llamada [Punk API](https://www.punkapi.com/).
--- main.js
+++ main.js
@@ -6,24 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const mostrarIBU = document.querySelector('.ibu')
     const mostrarEBC = document.querySelector('.ebc')
 
-    function recogerDatos(e) {
+    const recogerDatos = (e) => {
         e.preventDefault()
         fetch('https://api.punkapi.com/v2/beers/random')
         .then(response => {
             return response.json()
         })
-        .then(datos => {
-            const nombre = datos[0].name
-            const descripcion = datos[0].description
-            const abv = datos[0].abv
-            const ibu = datos[0].ibu
-            const ebc = datos[0].ebc
-            cervezaAleatoria.innerHTML = 'Nombre: ' + nombre
-            mostrarDescripcion.innerHTML = 'Descripción:  ' + descripcion
-            mostrarABV.innerHTML = 'ABV:  ' + abv
-            mostrarIBU.innerHTML = 'IBU:  ' + ibu
-            mostrarEBC.innerHTML = 'EBC:  ' + ebc
+        .then( ( [ {name, description, abv, ibu, ebc}]) => {
+            cervezaAleatoria.innerHTML = 'Nombre: ' + name
+            mostrarDescripcion.innerHTML = 'Descripción:  ' + description
+            mostrarABV.innerHTML = 'ABV (Alcohol por volumen) :  ' + abv
+            mostrarIBU.innerHTML = 'IBU (Unidad Internacional de Amargor) :  ' + ibu
+            mostrarEBC.innerHTML = 'EBC (Convención Europea Cervecera):  ' + ebc
         })
-    }
+    } 
+
     botonInicio.addEventListener('click', recogerDatos)
 })
